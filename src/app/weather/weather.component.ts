82c6f42eb4fc2eb2a649fbaf7fde36833949f4e2
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-08-31 20:38:53 -0400

    refactored weather component

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-weather',
@@ -13,12 +15,19 @@ export class WeatherComponent {
   iterations = 0;
   imagePath: string = '../../assets/06-s.png';
   weatherImg: string = '../../assets/06-s.png';
+  apiKey: string = environment.apiKey;
 
   constructor() {
     this.getWeather();
   }
 
-  getWeather() {
+  async getWeather() {
+    // const response = await axios.get(
+    //   `http://dataservice.accuweather.com/currentconditions/v1/32981${this.apiKey}`
+    // );
+    // this.weatherTitle = response.data[0].WeatherText;
+    // this.currentTemp = response.data[0].Temperature.Imperial.Value;
+
     const weatherObj = JSON.parse(localStorage.getItem('weather'));
     this.weatherTitle = weatherObj.data[0].WeatherText;
     this.currentTemp = weatherObj.data[0].Temperature.Imperial.Value;
